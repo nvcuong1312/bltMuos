b70895e1b3fc92c0f59b40bc598481e40b518aa8
cuongnv <cnv>
2024-12-31 18:26:29 +0900

    Add Input

---
+++ .bluetooth/input.lua
@@ -0,0 +1,70 @@
+local love = require("love")
+local input = {}
+
+local joystick
+local key = ""
+
+function input.load()
+    -- love.graphics.setFullscreen(true)
+    -- ScanDevices(5)
+
+    -- local devices = GetDevices()
+    -- for _, device in ipairs(devices) do
+    --     print("IP: " .. device.ip .. ", Name: " .. device.name)
+    -- end
+    -- Initialize joystick
+    local joysticks = love.joystick.getJoysticks()
+    if #joysticks > 0 then
+        joystick = joysticks[1]
+    end
+end
+
+function input.update(dt)
+    if joystick then
+        if joystick:isGamepadDown("dpleft") then
+            key = "left"
+        end
+        if joystick:isGamepadDown("dpright") then
+            key = "right"
+        end
+        if joystick:isGamepadDown("dpup") then
+            key = "up"
+        end
+        if joystick:isGamepadDown("dpdown") then
+            key = "down"
+        end
+        if joystick:isGamepadDown("a") then
+            key = "a"
+        end
+        if joystick:isGamepadDown("b") then
+            key = "b"
+        end
+        if joystick:isGamepadDown("x") then
+            key = "x"
+        end
+        if joystick:isGamepadDown("y") then
+            key = "y"
+        end
+        if joystick:isGamepadDown("back") then
+            key = "select"
+        end
+        if joystick:isGamepadDown("start") then
+            key = "start"
+        end
+        if joystick:isGamepadDown("leftshoulder") then
+            key = "leftshoulder"
+        end
+        if joystick:isGamepadDown("rightshoulder") then
+            key = "rightshoulder"
+        end
+    end
+end
+
+function input.onClick(callBack)
+    if key ~= "" then
+        callBack(key)
+        key = ""
+    end 
+end
+
+return input
--- .bluetooth/main.lua
+++ .bluetooth/main.lua
@@ -4,6 +4,11 @@ local Bluetooth = require("bluetooth")
 
 local textblock = require("textblock")
 
+
+local input = require("input")
+
+local keyPrs = ""
+
 function love.load()
     -- love.graphics.setFullscreen(true)
     -- ScanDevices(5)
@@ -12,19 +17,56 @@ function love.load()
     -- for _, device in ipairs(devices) do
     --     print("IP: " .. device.ip .. ", Name: " .. device.name)
     -- end
-    local height = love.graphics.getHeight( )
-    local width = love.graphics.getWidth( )
+    -- Initialize joystick
+
+    local height = love.graphics.getHeight()
+    local width = love.graphics.getWidth()
+    input.load()
     print("height: " .. height .. " width: " .. width)
 end
 
 function love.draw()
-    callBack = function() 
-        print("click click")
-    end
-    tbl = textblock("cuong", true, callBack)
+    -- callBack = function()
+    --     print("click click")
+    -- end
+    -- tbl = textblock("cuong", true, callBack)
 
+    love.graphics.print(keyPrs)
 end
 
 function love.update(dt)
-    love.graphics.print("ahihi", 400, 300)
+    input.update(dt)
+    input.onClick(function (key)
+        keyPrs = key
+    end)
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        keyPrs = "escape"
+    end
+
+    if key == "left" then
+        keyPrs = "left"
+    end
+
+    if key == "right" then
+        keyPrs = "right"
+    end
+
+    if key == "up" then
+        keyPrs = "up"
+    end
+
+    if key == "down" then
+        keyPrs = "down"
+    end
+
+    if key == "return" then
+        keyPrs = "return"
+    end
+
+    if key == "lalt" then
+        keyPrs = "lalt"
+    end
 end
