5ec50121285653604d05c98a3e4d5c3d72d8ad94
DESKTOP-8SUN3FE\CuongNV <cuongnv>
2025-03-01 14:38:25 +0700

    Auto start wlan

--- Bluetooth.sh
+++ Bluetooth.sh
@@ -17,6 +17,21 @@ BINDIR="$LOVEDIR/bin"
 export SDL_GAMECONTROLLERCONFIG_FILE="/usr/lib/gamecontrollerdb.txt"
 export LD_LIBRARY_PATH="$BINDIR/libs.aarch64:$LD_LIBRARY_PATH"
 
+if ! ifconfig wlan0 >/dev/null 2>&1; then
+	if ! lsmod | grep -wq "$(GET_VAR "device" "network/name")"; then
+		rmmod "$(GET_VAR "device" "network/module")"
+		sleep 1
+		modprobe --force-modversion "$(GET_VAR "device" "network/module")"
+		while [ ! -d "/sys/class/net/$(GET_VAR "device" "network/iface")" ]; do
+			sleep 1
+		done
+	fi
+
+	rfkill unblock all
+	ip link set "$(GET_VAR "device" "network/iface")" up
+	iw dev "$(GET_VAR "device" "network/iface")" set power_save off
+fi
+
 # Launcher
 cd "$LOVEDIR" || exit
 SET_VAR "system" "foreground_process" "love"
