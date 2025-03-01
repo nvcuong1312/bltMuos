69cb92bf431f83c0777aba7725a0ab55e5d24bb2
DESKTOP-8SUN3FE\CuongNV <cuongnv>
2025-03-01 14:46:21 +0700

    Update

--- Bluetooth.sh
+++ Bluetooth.sh
@@ -32,6 +32,12 @@ if ! ifconfig wlan0 >/dev/null 2>&1; then
 	iw dev "$(GET_VAR "device" "network/iface")" set power_save off
 fi
 
+modprobe /lib/modules/4.9.170/kernel/drivers/bluetooth/rtl_btlpm.ko
+rtk_hciattach -n -s 115200 /dev/ttyS1 rtk_h5 > /mnt/mmc/MUOS/log/rtk_hciattach.log 2>&1 &
+/usr/libexec/bluetooth/bluetoothd -n -d > /mnt/mmc/MUOS/log/bluetoothd.log 2>&1 &
+sleep 10 # YAY more hacks
+bluetoothctl power on
+
 # Launcher
 cd "$LOVEDIR" || exit
 SET_VAR "system" "foreground_process" "love"
