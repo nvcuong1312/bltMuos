87d3de33351c9469859ae35c6c75cc43b5fd5d3e
cuongnv <nvcuong1312@gmail.com>
2025-10-01 22:32:51 +0700

    Update

--- package/update_trimui.sh
+++ package/update_trimui.sh
@@ -3,7 +3,8 @@
 . /opt/muos/script/var/func.sh
 
 chmod +x /usr/bin/expect
-chmod 755 /bin/bluetoothctl /bin/hciattach /bin/hciconfig /bin/hcidump /bin/hcitool
+chmod +x /bin/bluetoothctl /bin/hciattach /bin/hciconfig /bin/hcidump /bin/hcitool
+chmod +x /usr/libexec/bluetooth/bluetoothd
 
 [ -f "/opt/muos/expect/usr/bin/expect" ] && rm -f "/opt/muos/expect/usr/bin/expect" 2>/dev/null
 [ -f "/opt/muos/expect/usr/bin/expect_autoexpect" ] && rm -f "/opt/muos/expect/usr/bin/expect_autoexpect" 2>/dev/null
